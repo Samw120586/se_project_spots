954f4440ee5d40ee96022559b8bccc8797af5cf6
Samuel Whitford <Samw120586@gmail.com>
2025-08-20 22:17:23 -0400

    Spots stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,24 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
-editProfileButton.addEventListener("click", function () {
+const addCardFormEl = newPostModal.querySelector(".modal__form");
+const nameInput = document.querySelector("#card-caption-input");
+const linkInput = document.querySelector("#card-image-input");
+
+  editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +33,19 @@ newPostButton.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleAddCardSubmit (evt) {
+  evt.preventDefault();
+  console.log();
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+addCardFormEl.addEventListener("submit", handleAddCardSubmit);
