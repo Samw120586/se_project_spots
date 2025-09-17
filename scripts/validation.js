ecc67a066f0ebb967c17ec18fc2f70ec7fc3d066
Samuel Whitford <Samw120586@gmail.com>
2025-09-16 23:49:34 -0400

    Spots stage 9 event listeners

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,1 +1,33 @@
-console.log(working);
+const showInputError = (inputElement, formElement, errorMessage) => {
+  const errorMessageID = inputElement.id + "-error";
+  const errorMessageElement = document.querySelector("#", errorMessageID);
+  errorMessageElement.textContent = errorMessage;
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelectorAll(".modal__submit-button");
+
+
+inputList.forEach((inputElement) => {
+  inputElement.addEventListener("input", function () {
+    checkInputValidity(formElement, inputElement);
+    toggleButtonState(inputList, buttonElement);
+  });
+});
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  })
+};
+
+enableValidation();
